b7992ef38fd393b5db93c8436ce8f6119de9de45
Prajwal Padal <54552389+prajwal4518@users.noreply.github.com>
2021-08-18 11:57:59 +0530

    work started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
-
+import photo from "./Styles/photo.jpg";
 function App() {
   return (
     <div className="App">
-      <h1>Prajwal's Portfolio</h1>
+      <div className="parent">
+        <div className="child">
+          <div className="box"></div>
+        </div>
+        <div className="child">
+          <img src={photo} alt="photo" />
+        </div>
+        <div className="child"></div>
+      </div>
+      {/* <div className="exm"></div> */}
     </div>
   );
 }
