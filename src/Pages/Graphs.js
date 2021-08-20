7445289b4dcde1a0b2dbe3aa0db86c7494edf928
Prajwal Padal <54552389+prajwal4518@users.noreply.github.com>
2021-08-20 12:57:57 +0530

    skill-section

---
+++ src/Pages/Graphs.js
@@ -0,0 +1,39 @@
+export const Graphs = [
+  {
+    graph1: (
+      <div className="box">
+        <div className="icon">
+          <p>ReactJs</p>
+        </div>
+        <div className="box-line"></div>
+      </div>
+    ),
+
+    graph2: (
+      <div className="box">
+        <div className="icon">
+          <p>Fluter</p>
+        </div>
+        <div className="box-line"></div>
+      </div>
+    ),
+
+    graph3: (
+      <div className="box">
+        <div className="icon">
+          <p>CSS</p>
+        </div>
+        <div className="box-line"></div>
+      </div>
+    ),
+
+    graph4: (
+      <div className="box">
+        <div className="icon">
+          <p>HTML</p>
+        </div>
+        <div className="box-line"></div>
+      </div>
+    ),
+  },
+];
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import photo from "../../Styles/photo.jpg";
 import "./SASS/HomepageStyle.css";
+import { Graphs } from "../Graphs.js";
 export const Homepage = () => {
   return (
     <div>
@@ -17,7 +18,27 @@ export const Homepage = () => {
         <div className="child">
           <img src={photo} alt="abcd" />
         </div>
-        <div className="child"></div>
+        <div className="child">
+          <div className="skill-sec">
+            <h5>My Skills</h5>
+            <p>
+              hello my name is praajwal dnyaneshwarrao padal, iam studying in
+              MGM JNEC College, Aurangabad
+            </p>
+            <div className="box-container">
+              {Graphs.map((graph, key) => {
+                return (
+                  <div key={key}>
+                    {graph.graph1}
+                    {graph.graph2}
+                    {graph.graph3}
+                    {graph.graph4}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
       </div>
       <div className="exm"></div>
     </div>
